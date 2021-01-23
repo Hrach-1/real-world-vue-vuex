b7cebad1bee29a45a67e0025b14a3bbd20c510f2
Hrach-1 <steepgaskin@gmail.com>
2021-01-23 19:24:54 +0400

    finish L6

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,22 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+      |
+    </template>
+
+    <template v-if="eventsTotal > page * 3">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page + 1 } }"
+        rel="next"
+        >Next Page</router-link
+      >
+    </template>
   </div>
 </template>
 
@@ -19,11 +35,11 @@ export default {
       page: this.page
     })
   },
-  computed:{
+  computed: {
     page() {
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events'])
+    ...mapState(['events', 'eventsTotal'])
   }
 }
 </script>
