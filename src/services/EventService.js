53301a6f6373f11acc5af1e5a9b979abc51a6226
Hrach-1 <steepgaskin@gmail.com>
2021-01-23 18:19:43 +0400

    L6_START

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `https://my-json-server.typicode.com/Hrach-1/real-world-vue-vuex/`,
   withCredentials: false, // This is the default
   headers: {
     Accept: 'application/json',
